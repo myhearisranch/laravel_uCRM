e66032d33d7b7b49c48550e75e6166a6ea3c735a
myhearisranch <myhearisranch@gmail.com>
2025-05-24 11:11:49 +0900

    Inertia_install

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Inertia: ビューファイルを読み込む時の書き方
+//Inertia::render('コンポーネント名(viewのファイル名)', [変数名])
 Route::get('/', function () {
-    return view('welcome');
+    //welcom: resources/js/pages/welcom.vueを指す
+    return Inertia::render('Welcome', [
+        //連想配列で4つ変数を渡している
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
