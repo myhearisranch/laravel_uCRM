60e4c435162ce9bd8e5abaac5abd6532f83696d5
myhearisranch <myhearisranch@gmail.com>
2025-05-25 15:47:42 +0900

    add:商品一覧ページの追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,10 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            ItemSeeder::class,
         ]);
+
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+
 
 class ItemSeeder extends Seeder
 {
@@ -14,6 +18,22 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('items')->insert([
+            [
+                'name' => 'カット',
+                'email' => 'カットの詳細',
+                'password' => 6000
+            ],
+            [
+                'name'     => 'カラー',
+                'email'    => 'カラーの詳細',
+                'password' => 8000
+            ],
+            [
+                'name'     => 'パーマ(カット込み)',
+                'email'    => 'パーマの詳細',
+                'password' => 6000
+            ]
+        ]);
     }
 }
